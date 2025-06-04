1e6a4d8a6b396de36e52b4c18a82ce8ffe33fe66
Martin Moreira <172557112+mimoreirac@users.noreply.github.com>
2025-06-03 23:12:48 -0500

    Working users script missing styles

--- script.js
+++ script.js
@@ -6,27 +6,25 @@ fetch("https://randomuser.me/api/?results=5")
   .then((response) => {
     return response.json();
   })
-  .then((data) => {
-    userList = data;
+  .then((users) => {
+    users.results.forEach((user) => {
+      const userCard = document.createElement("div");
+      const userName = document.createElement("h3");
+      const userCountry = document.createElement("p");
+      const userEmail = document.createElement("p");
+
+      userName.classList.add("user-name");
+      userName.textContent = `${user.name.first} ${user.name.last}`;
+      userCountry.classList.add("user-country");
+      userCountry.textContent = `${user.location.city}`;
+      userEmail.classList.add("user-email");
+      userCountry.textContent = `${user.email}`;
+      userCard.classList.add("user-card");
+
+      userCard.appendChild(userName);
+      userCard.appendChild(userCountry);
+      userCard.appendChild(userEmail);
+
+      cardContainer.appendChild(userCard);
+    });
   });
-
-userList.forEach((user) => {
-  const userCard = document.createElement("div");
-  const userName = document.createElement("h3");
-  const userCountry = document.createElement("p");
-  const userEmail = document.createElement("p");
-
-  userName.classList.add("user-name");
-  userName.textContent = `${user.name.first} ${user.name.last}`
-  userCountry.classList.add("user-country");
-  userCountry.textContent = `${user.location.country}`;
-  userEmail.classList.add("user-email");
-  userCountry.textContent = `${user.email}`;
-  userCard.classList.add("user-card");
-
-  userCard.appendChild(userName);
-  userCard.appendChild(userCountry);
-  userCard.appendChild(userEmail);
-  
-  cardContainer.appendChild(userCard);
-});
